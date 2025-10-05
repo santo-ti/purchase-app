e7c4b3e8f27368f0056028a3fa7e7926daf7cf01
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2025-10-04 23:45:18 -0300

    feat: Replace text with logo image in Home component

--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -3,12 +3,11 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
-  text: {
-    fontSize: 32,
-    fontWeight: "bold",
-    color: "red",
+  logo: {
+    height: 34,
+    width: 134,
   },
 });
