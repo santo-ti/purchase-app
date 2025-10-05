ed13555829f569302965d807584c7de698462c5e
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2025-10-05 00:47:50 -0300

    feat: Wrap Input and Button components in a form container in Home screen

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -8,8 +8,12 @@ export function Home() {
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-      <Input placeholder="O que você precisa comprar?" />
-      <Button title="Entrar" />
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content} />
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -6,11 +6,24 @@ export const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#D0D2D8",
     paddingTop: 62,
-    padding: 24,
-    gap: 24,
   },
   logo: {
     height: 34,
     width: 134,
+  },
+  form: {
+    width: "100%",
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    marginTop: 24,
   },
 });
